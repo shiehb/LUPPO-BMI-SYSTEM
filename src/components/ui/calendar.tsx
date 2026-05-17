c3b9fb29bb2c56d921ac3f97616fe0bcc2024831
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2026-05-17 12:35:40 +0800

    Update calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -43,7 +43,7 @@ function Calendar({
           date.toLocaleString(locale?.code, { month: "short" }),
         ...formatters,
       }}
-      classNames={{
+      classNames={({
         root: cn("w-fit", defaultClassNames.root),
         months: cn(
           "relative flex flex-col gap-4 md:flex-row",
@@ -132,7 +132,7 @@ function Calendar({
         ),
         hidden: cn("invisible", defaultClassNames.hidden),
         ...classNames,
-      }}
+      } as any)}
       components={{
         Root: ({ className, rootRef, ...props }) => {
           return (
